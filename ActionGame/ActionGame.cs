fe22d7989bacbbd9f83dff24a37ee36fbdc58010
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2025-12-27 19:59:00 +0100

    changing rooms works now
    Still gotta get rid of that camera jerking tho

--- ActionGame/ActionGame.cs
+++ ActionGame/ActionGame.cs
@@ -1,12 +1,15 @@
 using Godot;
 using System;
 using Common;
+using System.Net.Sockets;
 
 public partial class ActionGame : Control
 {
     private int CurrentRoomID = -1;
     private Room CurrentRoom;
     private Player Player;
+    private bool RecentReparent = false;
+
 
     public override void _Ready()
     {
@@ -14,24 +17,44 @@ public partial class ActionGame : Control
         Player.Initiate(this);
         Player.Position = new Vector2(0, 0);
         CurrentRoom = GetChild<Room>(0);
-        CurrentRoom.GetNode("EntityMap").AddChild(Player);
+        CurrentRoom.AddChild(Player);
 
         CustomEvents.Instance.PlayerChangedRoom += ChangeRoom;
     }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        // This check is needed or else the game crashes
+        if (RecentReparent)
+        {
+            RecentReparent = false;
+        }
+    }
+
 
     private void ChangeRoom(int roomID)
     {
         CurrentRoomID = roomID;
 
-        foreach (Room room in GetChildren())
+        foreach (var child in GetChildren())
         {
-            if (room.RoomID == CurrentRoomID)
+            if (child as Room != null)
             {
-                CurrentRoom = room;
+                var room = (Room)child;
+                if (room.RoomID == CurrentRoomID)
+                {
+                    CurrentRoom = room;
+                }
             }
+
         }
 
         GD.Print("Changed to room: " + CurrentRoomID.ToString());
+        if (!RecentReparent && Player.GetParent() != CurrentRoom)
+        {
+            Player.CallDeferred(Node2D.MethodName.Reparent, CurrentRoom);
+            RecentReparent = true;
+        }
     }
 
     public void Spawn(Node2D levelObject, Vector2 pos)
