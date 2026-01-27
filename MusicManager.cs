fe6fcb8da0b64e33e54663b33771aeac5a0d6819
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2026-01-27 10:16:43 +0100

    music pauses rather than resetting

--- MusicManager.cs
+++ MusicManager.cs
@@ -1,17 +1,32 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class MusicManager : Node
 {
     static AudioStreamPlayer Level, BreakRoom;
     static AudioStreamPlayer ActiveSong;
+    private static Dictionary<string, double> SongTimers = new Dictionary<string, double>();
+    private static string ActiveSongName;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        SongTimers["Level"] = 0;
+        SongTimers["Silence"] = 0;
+        SongTimers["BreakRoom"] = 0;
         Level = GetNode<AudioStreamPlayer>("Level");
         BreakRoom = GetNode<AudioStreamPlayer>("BreakRoom");
         ActiveSong = GetNode<AudioStreamPlayer>("Silence");
         SwitchTrack("Level");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (ActiveSongName != "BreakRoom")
+        {
+            SongTimers[ActiveSongName] += delta;
+        }
+
     }
 
     public static void SwitchTrack(string trackName)
@@ -29,9 +44,10 @@ public partial class MusicManager : Node
 
         if (newSong != ActiveSong)
         {
-            ActiveSong.Playing = false;
+            ActiveSongName = trackName;
+            ActiveSong.Stop();
             ActiveSong = newSong;
-            ActiveSong.Play();
+            ActiveSong.Play((float)SongTimers[ActiveSongName]);
         }
 
 
